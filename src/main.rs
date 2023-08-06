ceef3dba8baaec703e691c1debaca11dbe9516b8
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-08-06 23:41:48 +0100

    Migrated gallery with load from directory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use rocket_dyn_templates::{Template, context};
 use rocket::fs::FileServer;
 
+use std::fs;
+
 #[macro_use] extern crate rocket;
+
+fn get_gallery_paths(location: &str) -> Vec<std::string::String> {
+    let dir_path = format!("./static/img/Gallery/{}", location);
+    return fs::read_dir(dir_path)
+    .unwrap()
+    .filter_map(|e| e.ok())
+    .map(|e| e.path().file_stem().expect("jpegs").to_string_lossy().into_owned())
+    .collect::<Vec<_>>();
+}
 
 #[get("/")]
 fn index() -> Template {
@@ -11,6 +22,13 @@ fn index() -> Template {
 #[get("/projects")]
 fn projects() -> Template {
     Template::render("projects", context! {})
+}
+
+#[get("/gallery")]
+fn gallery() -> Template {
+    let paths_northumberland = get_gallery_paths("Northumberland");
+    let paths_wales = get_gallery_paths("Wales");
+    Template::render("gallery", context! {paths_wales, paths_northumberland})
 }
 
 #[get("/cv")]
@@ -26,7 +44,7 @@ fn contact() -> Template {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, projects, cv, contact])
+        .mount("/", routes![index, projects, gallery, cv, contact])
         .mount("/static", FileServer::from("static"))
         .attach(Template::fairing())
 }
