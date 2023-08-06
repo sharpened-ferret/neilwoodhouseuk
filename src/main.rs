539259e2a1548b776074b40787c364db3bc7cce5
Neil Woodhouse <neiltwoodhouse@gmail.com>
2023-08-07 00:26:21 +0100

    Migrated remaining projects

--- src/main.rs
+++ src/main.rs
@@ -41,15 +41,53 @@ fn contact() -> Template {
     Template::render("contact", context! {})
 }
 
+
+// Project Pages
 #[get("/projects/pathfinding")]
 fn projects_pathfinding() -> Template {
     Template::render("projects/pathfinding", context! {})
+}
+#[get("/projects/allendale-holidays-website")]
+fn projects_allendale_holidays() -> Template {
+    Template::render("projects/allendale-holidays-website", context! {})
+}
+#[get("/projects/bae-systems-ctf")]
+fn projects_bae_systems_ctf() -> Template {
+    Template::render("projects/bae-systems-ctf", context! {})
+}
+#[get("/projects/battlefield-labyrinth")]
+fn projects_battlefield_labyrinth() -> Template {
+    Template::render("projects/battlefield-labyrinth", context! {})
+}
+#[get("/projects/festival-of-code")]
+fn projects_festival_of_code() -> Template {
+    Template::render("projects/festival-of-code", context! {})
+}
+#[get("/projects/google-hashcode")]
+fn projects_google_hashcode() -> Template {
+    Template::render("projects/google-hashcode", context! {})
+}
+#[get("/projects/lora-chat")]
+fn projects_lora_chat() -> Template {
+    Template::render("projects/lora-chat", context! {})
+}
+#[get("/projects/recipe-viewer")]
+fn projects_recipe_viewer() -> Template {
+    Template::render("projects/recipe-viewer", context! {})
+}
+#[get("/projects/personal-website")]
+fn projects_personal_website() -> Template {
+    Template::render("projects/personal-website", context! {})
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, projects, gallery, cv, contact, projects_pathfinding])
+        .mount("/", routes![
+            index, projects, gallery, cv, contact, 
+            projects_pathfinding, projects_allendale_holidays, projects_bae_systems_ctf, projects_battlefield_labyrinth, projects_festival_of_code, 
+            projects_google_hashcode, projects_lora_chat, projects_recipe_viewer, projects_personal_website
+        ])
         .mount("/static", FileServer::from("static"))
         .attach(Template::fairing())
 }
